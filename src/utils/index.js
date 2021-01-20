ac4e70f5d3c0874a41b6ee6b11601e76e6722369
Peter Trofimov <ptrofimov7@gmail.com>
2021-01-20 21:38:35 +0200

    Added utils

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+
+export const entityRaces = {
+    Hobbit: 'Hobbit',
+    Human: 'Human',
+    Dworf: 'Dworf',
+    Elf: 'Elf'
+};
+
+
+export const entityColors = {
+    Hobbit: '4285B4',
+    Human: '3EB489',
+    Dworf: 'B5A642',
+    Elf: '7442C8'
+};
